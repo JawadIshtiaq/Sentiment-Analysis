f28da37be8be4ab708e056ac06776cfbf134bd85
Jawad Ishtiaq <100146296+JawadIshtiaq@users.noreply.github.com>
2023-08-19 16:07:17 +0500

    Update Sentiment_Analysis.py
    Cleaning code

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -1,24 +1,16 @@
-#Project Sentiment Analysis:
+# Project Sentiment Analysis:
  
 # It is a project to recognize the partiality 
 # of a sentence or paragraph whether it is a positive or
 # a negative comment.
                                
-
-
 from textblob import TextBlob
 from newspaper import Article
 from tkinter import simpledialog
 from tkinter.messagebox import showinfo
 import nltk
 
-# x = input('Enter any sentence to find its partiality:')
-# article = Article(url)
 answer = simpledialog.askstring('Sentiment Analysis', 'Enter any Sentence:\t\t\t\t')
-# print(x)
-# article.download()
-# article.parse()
-# article.nlp()
 
 text =answer
 blob = TextBlob(text)
@@ -34,10 +26,3 @@ elif y < 0:
 else:
     showinfo("Polarity", "The given value is either 'Null' or has 'No Partiality' 😐")    
 
-# print(z + "%" + " Partiality")
-# if sentiment < 0:
-#     print('It is a negative text')
-# elif sentiment > 0:
-#     print('it is a positive text')
-# elif sentiment == 0:
-#     print('Its a neutral text')
